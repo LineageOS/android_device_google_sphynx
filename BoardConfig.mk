0532585480ed78e6c36430fa591eebfd1e4bd3dd
followmsi <followmsi@gmail.com>
2017-11-03 15:09:28 +0100

    sphynx: Specify our kernel is called Image.fit
    Change-Id: Ie18cb7ab7c6e2a46b0920a01908787522765e4c3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ KERNEL_TOOLCHAIN_PREFIX        := aarch64-buildroot-linux-gnu-
 TARGET_KERNEL_SOURCE           := kernel/nvidia/kernel-$(TARGET_TEGRA_KERNEL)
 TARGET_KERNEL_CONFIG           := tegra_android_defconfig
 TARGET_KERNEL_RECOVERY_CONFIG  := tegra_android_recovery_defconfig
-BOARD_KERNEL_IMAGE_NAME        := Image.gz
+BOARD_KERNEL_IMAGE_NAME        := Image.fit
 TARGET_KERNEL_ADDITIONAL_FLAGS := "NV_BUILD_KERNEL_OPTIONS=$(TARGET_TEGRA_KERNEL)"
 include device/google/sphynx/modules.mk
 
