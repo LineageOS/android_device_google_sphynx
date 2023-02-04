ee6236968f569e08d302d3c7abd01b2cf61d0b87
electimon <electimon@gmail.com>
2023-02-05 01:10:47 +0800

    sphynx: properties: Set ro.vendor.lineage.tegra.model for USB
    Change-Id: I28d2a2f619f3b63fd467ab230ae685d216e43a88
    Signed-off-by: electimon <electimon@gmail.com>

--- properties.mk
+++ properties.mk
@@ -17,6 +17,7 @@ PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
 
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.lineage.tegra.model=Pixel C \
     ro.vendor.nv.usb.pid.adb=7104 \
     ro.vendor.nv.usb.pid.accessory.adb=7105 \
     ro.vendor.nv.usb.pid.audio_source.adb=7106 \
