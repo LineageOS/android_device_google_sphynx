f0c02679135f26c5ccddb4931a55f90f663c807d
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-29 13:30:49 -0400

    sphynx: Fold Lineage specific flags into standard configuration
    * We rely on Lineage platform stuff anyway.
    
    Change-Id: I733d08e6385481d4a22d6d51aa68539f90e12974

--- lineage_sphynx.mk
+++ lineage_sphynx.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit Sphynx-specific LineageOS additions.
-include device/google/sphynx/lineage.mk
-
 # Inherit some common AOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
