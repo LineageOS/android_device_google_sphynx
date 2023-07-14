680f271a610a9547688e5e7425398a4e0c14cd73
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-14 13:21:48 -0400

    sphynx: Disable CEC correctly
    Change-Id: I8da0046f22a54830161f50ee0b893badaf404b9a

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ TARGET_TEGRA_VARIANT    := common
 
 TARGET_TEGRA_BT       := bcm
 TARGET_TEGRA_CAMERA   := rel-shield-r
+TARGET_TEGRA_CEC      :=
 TARGET_TEGRA_KERNEL   := 4.9
 TARGET_TEGRA_WIDEVINE := rel-shield-r
 TARGET_TEGRA_WIFI     := bcm
