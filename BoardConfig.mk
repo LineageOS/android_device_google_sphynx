3649b6b684d331faab3d9c38d35a6e5f92d9d24c
Nolen Johnson <johnsonnolen@gmail.com>
2023-10-14 00:00:14 -0400

    sphynx: Set variant defconfig
    Change-Id: Ieed056e5d3cf62ad5f5d6a12a679489b437aff05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ TARGET_KERNEL_CONFIG           := tegra_android_defconfig
 TARGET_KERNEL_RECOVERY_CONFIG  := tegra_android_recovery_defconfig
 BOARD_KERNEL_IMAGE_NAME        := Image.fit
 TARGET_KERNEL_ADDITIONAL_FLAGS := "NV_BUILD_KERNEL_OPTIONS=$(TARGET_TEGRA_KERNEL)"
+TARGET_KERNEL_VARIANT_CONFIG := smaug_variant_defconfig
 include device/google/sphynx/modules.mk
 
 # Recovery
