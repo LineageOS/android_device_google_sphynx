b913ceb9a9ac8e309c86eff450e1c2dc3b80db0d
Aaron Kling <webgeek1234@gmail.com>
2023-11-23 00:05:56 -0600

    sphynx: extract: Get nvram and bpmp firmware
    Change-Id: I109e8561af6f33e2c950edf38ee554dd6bcb1f33

--- extract/patches.sh
+++ extract/patches.sh
@@ -1,20 +1,24 @@
-#
 # Copyright (C) 2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-SOURCE_PATHS+=("google/sphynx");
-FILELIST_PATHS+=("google/sphynx");
-PATCH_PATHS+=("google/sphynx");
-HOOK_PATHS+=("google/sphynx");
+# The previous default crashes the current firmware
+function patch_smaug_nvram() {
+  echo -n "Patching nvram default ccodes...";
+
+  sed -i 's/ccode=0/ccode=XY/'  ${LINEAGE_ROOT}/vendor/google/sphynx/opm8/bcm_firmware/bcm4354/bcmdhd.cal
+
+  echo "";
+}
+
+patch_smaug_nvram;
