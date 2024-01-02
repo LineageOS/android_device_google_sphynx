LineageOS/android_device_nvidia_quill

Disable system as root
Change-Id: I2cbeba371809ccb97e9120b2f1a0e3cf4e8cf844

initfiles/Android.mk
@@ -8,6 +8,15 @@ LOCAL_MODULE           := fstab.$(strip $(1))
 LOCAL_MODULE_CLASS     := ETC
 LOCAL_SRC_FILES        := fstab.quill
 LOCAL_VENDOR_MODULE    := true
+LOCAL_REQUIRED_MODULES := fstab.$(strip $(1))_ramdisk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.$(strip $(1))_ramdisk
+LOCAL_MODULE_STEM   := fstab.$(strip $(1))
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.quill
+LOCAL_MODULE_PATH   := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

LineageOS/android_device_google_sphynx

initfiles/Android.mk
@@ -19,6 +19,15 @@ LOCAL_MODULE        := fstab.dragon
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := fstab.dragon
 LOCAL_VENDOR_MODULE := true
+LOCAL_REQUIRED_MODULES := fstab.dragon_ramdisk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.dragon_ramdisk
+LOCAL_MODULE_STEM   := fstab.dragon
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.dragon
+LOCAL_MODULE_PATH   := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)