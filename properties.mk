efc46d1a94b66635579e97edcd456f89b84a8eac
Nolen Johnson <johnsonnolen@gmail.com>
2024-04-07 01:18:38 -0400

    sphynx: Update USB IDs
    * Reuse stock's PID for Accessory + Audio Source for
      stuff stock doesn't support.
    
    Change-Id: I6e42c1dc0da741edd4b759d64c4a52ea2a1ce870
    (cherry picked from commit 726d143eff27980a5ebbb2166ca26ee1debbbcd5)

--- properties.mk
+++ properties.mk
@@ -17,6 +17,7 @@ PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
 
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
+<<<<<<< HEAD   (bd82bb sphynx: sepolicy: Initial support)
     ro.vendor.nv.usb.pid.adb=7104 \
     ro.vendor.nv.usb.pid.accessory.adb=7105 \
     ro.vendor.nv.usb.pid.audio_source.adb=7106 \
@@ -34,5 +35,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.nv.usb.pid.rndis.acm.adb=AF00 \
     ro.vendor.nv.usb.pid.rndis.adb=2000 \
     ro.vendor.nv.usb.vid=057E \
+=======
+    ro.vendor.lineage.tegra.model=Pixel C \
+    ro.vendor.nv.usb.pid.adb=4EE7 \
+    ro.vendor.nv.usb.pid.accessory.adb=2D01\
+    ro.vendor.nv.usb.pid.audio_source.adb=2D03 \
+    ro.vendor.nv.usb.pid.ecm=2D04 \
+    ro.vendor.nv.usb.pid.ecm.adb=2D05 \
+    ro.vendor.nv.usb.pid.midi=2D04 \
+    ro.vendor.nv.usb.pid.midi.adb=2D05 \
+    ro.vendor.nv.usb.pid.mtp=2D04 \
+    ro.vendor.nv.usb.pid.mtp.adb=2D05 \
+    ro.vendor.nv.usb.pid.ncm=2D04 \
+    ro.vendor.nv.usb.pid.ncm.adb=2D05 \
+    ro.vendor.nv.usb.pid.ptp=2D04 \
+    ro.vendor.nv.usb.pid.ptp.adb=2D05 \
+    ro.vendor.nv.usb.pid.rndis=2D04 \
+    ro.vendor.nv.usb.pid.rndis.acm.adb=2D05 \
+    ro.vendor.nv.usb.pid.rndis.adb=2D05 \
+    ro.vendor.nv.usb.vid=18D1 \
+>>>>>>> CHANGE (726d14 sphynx: Update USB IDs)
     sys.usb.controller=700d0000.xudc \
     vendor.sys.usb.udc=700d0000.xudc
