1c7e81d6a37ad97f28c0a8a6c734872e27d989ff
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    sphynx: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,4 +80,7 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 BOARD_VNDK_VERSION                     := current
 PRODUCT_FULL_TREBLE_OVERRIDE           := true
 
+# Updater
+AB_OTA_UPDATER := false
+
 include device/nvidia/t210-common/BoardConfigCommon.mk
