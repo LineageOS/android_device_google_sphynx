a52f217aeedc4cf1a12ed4cbd28f288e70555f1f
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-22 11:07:47 -0500

    Revert "sphynx: Fold vboot needs into device tree"
    This reverts commit 000a6bfa6765802ad8cc62a9815ffe6a5f5defb8.
    
    Reason: We're going to have to restore platform support anyway.
    Change-Id: I2c6984ede0e722cf363967a5e4cb84748a409c8f

--- device.mk
+++ device.mk
@@ -119,12 +119,7 @@ PRODUCT_PACKAGES += \
 endif
 
 # VBoot
-PRODUCT_SUPPORTS_VBOOT := true
-# The dev key is used to sign boot and recovery images.
-# We expect this file to exist with the suffixes ".vbprivk" and ".vbpupk".
-# TODO: find a proper location for this
-PRODUCT_VBOOT_SIGNING_KEY := external/vboot_reference/tests/devkeys/kernel_data_key
-PRODUCT_VBOOT_SIGNING_SUBKEY := external/vboot_reference/tests/devkeys/kernel_subkey
+$(call inherit-product, build/target/product/vboot.mk)
 
 # WiFi
 PRODUCT_PACKAGES += \
