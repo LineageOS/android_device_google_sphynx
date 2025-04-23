a6270ce1bfbd22ba0962d7854d7b29b8849faeb5
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-22 21:13:56 -0400

    sphynx: Drop outdated BPF property
    Change-Id: I011b304624aa5a5d59228c05e5a47361b2d91ee6

--- properties.mk
+++ properties.mk
@@ -6,10 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.device.default_name=Pixel C
 
-# Bpf
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.ebpf.supported=1
-
 # Charger
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     persist.sys.NV_ECO.IF.CHARGING=false
